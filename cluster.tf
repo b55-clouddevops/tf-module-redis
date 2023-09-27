148019d0969bb4d71b4da354e410c7a57a783531
CodingManoj <mvp2612@hotmail.com>
2023-09-26 22:28:22 -0400

    Git Auto Push

--- cluster.tf
+++ cluster.tf
@@ -7,7 +7,7 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = aws_elasticache_parameter_group.default.name
   subnet_group_name    = aws_elasticache_subnet_group.redis.name
   security_group_ids   = [aws_security_group.allows_redis.id]
-  engine_version       = "6.2"
+  engine_version       = "6.x"
   port                 = 6379
 }
 
